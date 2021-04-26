025ccf971494a20a6af471a68db25a249bf5451c
Emeka Mbachu <victor.c.mbachu@gmail.com>
2021-04-26 13:38:53 +0100

    styling with css classes

--- styling-with-css-classes.js
+++ styling-with-css-classes.js
@@ -1,6 +1,26 @@
 new Vue({
     el: '#app',
     data: {
-        color: 'blue'
+        color: 'blue',
+        styles: {
+            'background-color': 'blue'
+        },
+        shapes: [
+            {shape: 'circle'},
+            {shape: 'square'},
+            {shape: 'triangle', direction: 'up'},
+            {shape: 'triangle', direction: 'right'},
+            {shape: 'triangle', direction: 'down'},
+            {shape: 'triangle', direction: 'left'},
+        ]
+    },
+    methods: {
+        changeColor: function () {
+            if(this.color === 'blue'){
+                this.color = 'red';
+            }else{
+                this.color = 'blue';
+            }
+        }
     }
 })
